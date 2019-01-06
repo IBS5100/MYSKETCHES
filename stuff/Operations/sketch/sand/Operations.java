27d8997538bd76bb1db55cca597eeb6ffdf2a0f0
angel8818 <42220654+angel8818@users.noreply.github.com>
2019-01-06 18:49:18 -0500

    remember that the '+' operator acts as String concatenation as soon as a String, that is, anything in quotes, is concatenated, so 3 + 5 + . + 6 + 7 comes out to 8.67

---
+++ stuff/Operations/sketch/sand/Operations.java
@@ -0,0 +1,15 @@
+package sketch.sand;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Operations {
+	public static void main(String[] args) {
+		System.out.println(13 + 2 * 5 / 3);
+		System.out.println(2.5 * 2 * 5 / 10 + 1.5);
+		System.out.println(85 % 10 + 4 % 10 - 17 % 3);
+		System.out.println(2 + 3 + "." + (3 + 4) + 2 * 3);
+		System.out.println(482 / 10 / 5 / 2.0 * 2 + 14 / 5);
+	}
+}
